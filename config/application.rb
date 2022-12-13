919466613b06ab26cd378c98694ff0e47b3e52b2
d-brand <kawasaki@d-brand.jp>
2022-12-13 18:57:21 +0900

    lolipop用

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Team
     config.i18n.default_locale = :ja
     config.assets.initialize_on_precompile = false
     config.time_zone = 'Tokyo'
-    config.hosts << 'https://dbrandscedule.onrender.com'
+    #config.hosts << 'https://dbrandscedule.onrender.com'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

9f419110c8480be4b88d51b386284222c7b639b4
d-brand <kawasaki@d-brand.jp>
2022-12-16 15:42:16 +0900

    changeGemfile.lock

--- config/puma.rb
+++ config/puma.rb
@@ -35,7 +35,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # preload_app!
 #render.comデプロイ対応>>>>>>
 preload_app!
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+#workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 #render.comデプロイ対応<<<<<<
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
